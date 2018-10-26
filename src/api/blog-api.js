c407af0bbdc24489a8a68a1090b32716b62d9c53
y3tu <14643092@qq.com>
2018-10-27 00:56:27 +0800

    增加网页音乐播放

---
+++ src/api/blog-api.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+const service = axios.create({
+    baseURL: "http://localhost:8081",
+    withCredentials: true, // 跨域请求，允许保存cookie
+    timeout: 30000 // 请求超时时间
+});
+
+export const getWebsite = () => {
+    return service({
+        url: '/website/getAll',
+        method: 'get'
+    });
+};
+
+export const getMusic = () => {
+    return service({
+        method: 'get',
+        url: '/music/getAll'
+    });
+};
